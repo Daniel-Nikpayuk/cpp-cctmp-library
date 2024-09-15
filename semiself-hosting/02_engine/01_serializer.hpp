34882d3ce775572d28c5eba9cea9ee7cd79cd459
Daniel Nikpayuk <djharris@ualberta.ca>
2024-09-15 16:37:43 -0600

    [incomplete] initial support for the corpus data structure

--- semiself-hosting/02_engine/01_serializer.hpp
+++ semiself-hosting/02_engine/01_serializer.hpp
@@ -112,16 +112,14 @@ namespace engine {
 				using base::not_equal;
 				using base::equal;
 
-				nik_ce text_ctype_ptr origin(size_ctype n) const { return text.citer(start(n)); }
-				nik_ce text_ctype_ptr cbegin(size_ctype n) const { return text.citer(start(n)); }
+				nik_ce text_ctype_ptr origin (size_ctype n) const { return text.citer(start(n)); }
+				nik_ce text_ctype_ptr cbegin (size_ctype n) const { return text.citer(start(n)); }
 
-				nik_ce text_ctype_ptr citer(size_ctype n, size_ctype m) const
-					{ return cbegin(n) + m; }
+				nik_ce text_ctype_ptr citer (size_ctype n, size_ctype m) const { return cbegin(n) + m; }
+				nik_ce text_ctype_ref cat   (size_ctype n, size_ctype m) const { return cbegin(n)[m]; }
 
 				nik_ce size_type left_size(size_ctype n, text_ctype_cptr i) const
 					{ return i - cbegin(n); }
-
-				nik_ce text_ctype_ref at(size_ctype n, size_ctype m) const { return cbegin(n)[m]; }
 
 				template<typename In, typename End>
 				nik_ce bool not_equal(size_ctype n, In in, End end) const
@@ -278,9 +276,8 @@ namespace engine {
 				nik_ce text_type_ptr end   (size_ctype n) { return base::text.iter(base::finish(n)); }
 				nik_ce text_type_ptr last  (size_ctype n) { return end(n) - 1; }
 
-				nik_ce text_type_ref at(size_ctype n, size_ctype m) { return begin(n)[m]; }
-
-				nik_ce text_type_ptr iter(size_ctype n, size_ctype m) { return begin(n) + m; }
+				nik_ce text_type_ptr iter (size_ctype n, size_ctype m) { return begin(n) + m; }
+				nik_ce text_type_ref at   (size_ctype n, size_ctype m) { return begin(n)[m]; }
 
 				template<typename In, typename End>
 				nik_ce void push(In in, End end)
@@ -467,16 +464,21 @@ namespace engine {
 
 			size_type initial;
 			size_type terminal;
+			size_type length;
 
 		public:
 
-			nik_ce corpus_entry() : initial{}, terminal{} { }
+			nik_ce corpus_entry() : initial{}, terminal{}, length{} { }
 
-			nik_ce size_type start  () const { return initial; }
-			nik_ce size_type finish () const { return terminal; }
+			nik_ce size_type start    () const { return initial; }
+			nik_ce size_type finish   () const { return terminal; }
+			nik_ce size_type capacity () const { return length; }
 
-			nik_ce void set_start  (size_ctype i) { initial = i; }
-			nik_ce void set_finish (size_ctype t) { terminal = t; }
+			nik_ce void set_start    (size_ctype i) { initial = i; }
+			nik_ce void set_finish   (size_ctype t) { terminal = t; }
+			nik_ce void set_capacity (size_ctype l) { length = l; }
+
+			nik_ce void upsize(size_ctype n = 1) { terminal += n; } // needs to test capacity.
 	};
 
 /***********************************************************************************************************************/
@@ -524,8 +526,15 @@ namespace engine {
 			page_type page;
 			text_type text;
 
-			nik_ce size_type start  (size_ctype n) const { return base::operator [] (n).start(); }
-			nik_ce size_type finish (size_ctype n) const { return base::operator [] (n).finish(); }
+			nik_ce size_type start    (size_ctype n) const { return base::operator [] (n).start(); }
+			nik_ce size_type finish   (size_ctype n) const { return base::operator [] (n).finish(); }
+			nik_ce size_type capacity (size_ctype n) const { return base::operator [] (n).capacity(); }
+
+			nik_ce size_type start(size_ctype n, size_ctype m) const
+				{ return page[start(n) + m].start(); }
+
+			nik_ce size_type finish(size_ctype n, size_ctype m) const
+				{ return page[start(n) + m].finish(); }
 
 		public:
 
@@ -538,62 +547,290 @@ namespace engine {
 
 			// initial:
 
-			//	using base::origin;
-			//	using base::cbegin;
-			//	using base::citer;
-			//	using base::left_size;
-			//	using base::not_equal;
-			//	using base::equal;
+				using base::origin;
+				using base::cbegin;
+				using base::citer;
+				using base::left_size;
+				using base::not_equal;
+				using base::equal;
 
-			//	nik_ce text_ctype_ptr origin(size_ctype n) const { return text.citer(start(n)); }
-			//	nik_ce text_ctype_ptr cbegin(size_ctype n) const { return text.citer(start(n)); }
+				// page:
 
-			//	nik_ce text_ctype_ptr citer(size_ctype n, size_ctype m) const
-			//		{ return cbegin(n) + m; }
+				nik_ce page_ctype_ptr origin (size_ctype n) const { return page.citer(start(n)); }
+				nik_ce page_ctype_ptr cbegin (size_ctype n) const { return page.citer(start(n)); }
 
-			//	nik_ce size_type left_size(size_ctype n, text_ctype_cptr i) const
-			//		{ return i - cbegin(n); }
+				nik_ce page_ctype_ptr citer (size_ctype n, size_ctype m) const { return cbegin(n) + m; }
+				nik_ce page_ctype_ref cat   (size_ctype n, size_ctype m) const { return cbegin(n)[m]; }
 
-			//	nik_ce text_ctype_ref at(size_ctype n, size_ctype m) const { return cbegin(n)[m]; }
+				nik_ce size_type left_size(size_ctype n, page_ctype_cptr i) const
+					{ return i - cbegin(n); }
 
-			//	template<typename In, typename End>
-			//	nik_ce bool not_equal(size_ctype n, In in, End end) const
-			//		{ return text.not_equal(start(n), in, end); }
+				// text:
 
-			//	template<typename In, typename End>
-			//	nik_ce bool equal(size_ctype n, In in, End end) const
-			//		{ return text.equal(start(n), in, end); }
+				nik_ce text_ctype_ptr origin(size_ctype n, size_ctype m) const
+					{ return text.citer(start(n, m)); }
+
+				nik_ce text_ctype_ptr cbegin(size_ctype n, size_ctype m) const
+					{ return text.citer(start(n, m)); }
+
+				nik_ce text_ctype_ptr citer(size_ctype n, size_ctype m, size_ctype l) const
+					{ return cbegin(n, m) + l; }
+
+				nik_ce text_ctype_ref cat(size_ctype n, size_ctype m, size_ctype l) const
+					{ return cbegin(n, m)[l]; }
+
+				nik_ce size_type left_size(size_ctype n, size_ctype m, text_ctype_cptr i) const
+					{ return i - cbegin(n, m); }
+
+				template<typename In, typename End>
+				nik_ce bool not_equal(size_ctype n, size_ctype m, In in, End end) const
+					{ return text.not_equal(start(n, m), in, end); }
+
+				template<typename In, typename End>
+				nik_ce bool equal(size_ctype n, size_ctype m, In in, End end) const
+					{ return text.equal(start(n, m), in, end); }
 
 			// terminal:
 
-			//	using base::size;
-			//	using base::max;
-			//	using base::is_empty;
-			//	using base::not_empty;
-			//	using base::cend;
-			//	using base::clast;
-			//	using base::right_size;
+				using base::size;
+				using base::max;
+				using base::is_empty;
+				using base::not_empty;
+				using base::cend;
+				using base::clast;
+				using base::right_size;
 
-			//	nik_ce size_type size (size_ctype n) const { return finish(n) - start(n); }
-			//	nik_ce size_type max  (size_ctype n) const { return size(n) - 1; }
+				// page:
 
-			//	nik_ce bool is_empty  (size_ctype n) const { return (size(n) == 0); }
-			//	nik_ce bool not_empty (size_ctype n) const { return (size(n) != 0); }
+				nik_ce size_type size (size_ctype n) const { return finish(n) - start(n); }
+				nik_ce size_type max  (size_ctype n) const { return size(n) - 1; }
 
-			//	nik_ce text_ctype_ptr cend  (size_ctype n) const { return text.citer(finish(n)); }
-			//	nik_ce text_ctype_ptr clast (size_ctype n) const { return cend(n) - 1; }
+				nik_ce bool is_empty  (size_ctype n) const { return (size(n) == 0); }
+				nik_ce bool not_empty (size_ctype n) const { return (size(n) != 0); }
 
-			//	nik_ce size_type right_size(size_ctype n, text_ctype_cptr i) const
-			//		{ return cend(n) - i; }
+				nik_ce page_ctype_ptr cend  (size_ctype n) const { return page.citer(finish(n)); }
+				nik_ce page_ctype_ptr clast (size_ctype n) const { return cend(n) - 1; }
+
+				nik_ce size_type right_size(size_ctype n, page_ctype_cptr i) const
+					{ return cend(n) - i; }
+
+				// text:
+
+				nik_ce size_type size(size_ctype n, size_ctype m) const
+					{ return finish(n, m) - start(n, m); }
+
+				nik_ce size_type max(size_ctype n, size_ctype m) const { return size(n, m) - 1; }
+
+				nik_ce bool is_empty  (size_ctype n, size_ctype m) const { return (size(n, m) == 0); }
+				nik_ce bool not_empty (size_ctype n, size_ctype m) const { return (size(n, m) != 0); }
+
+				nik_ce text_ctype_ptr cend(size_ctype n, size_ctype m) const
+					{ return text.citer(finish(n, m)); }
+
+				nik_ce text_ctype_ptr clast(size_ctype n, size_ctype m) const
+					{ return cend(n, m) - 1; }
+
+				nik_ce size_type right_size(size_ctype n, size_ctype m, text_ctype_cptr i) const
+					{ return cend(n, m) - i; }
 	};
 
 /***********************************************************************************************************************/
 
 // literal:
 
+	template<typename Body, typename Page, typename Text, typename SizeType>
+	class corpus_literal	: public protocorpus
+				<
+					cctmp::array_literal < Body , SizeType >,
+					cctmp::array_literal < Page , SizeType >,
+					cctmp::array_literal < Text , SizeType >
+				>
+	{
+		public:
+
+			using base		= protocorpus
+						<
+							cctmp::array_literal < Body , SizeType >,
+							cctmp::array_literal < Page , SizeType >,
+							cctmp::array_literal < Text , SizeType >
+						>;
+
+			using type		= typename base::type;
+			using type_ptr		= typename base::type_ptr;
+			using type_cptr		= typename base::type_cptr;
+			using type_ref		= typename base::type_ref;
+
+			using ctype		= typename base::ctype;
+			using ctype_ptr		= typename base::ctype_ptr;
+			using ctype_cptr	= typename base::ctype_cptr;
+			using ctype_ref		= typename base::ctype_ref;
+
+			using size_type		= typename base::size_type;
+			using size_ctype	= typename base::size_ctype;
+
+			using page_type		= typename base::page_type;
+			using page_type_ptr	= typename base::page_type_ptr;
+			using page_type_ref	= typename base::page_type_ref;
+			using page_ctype_ptr	= typename base::page_ctype_ptr;
+			using page_ctype_cptr	= typename base::page_ctype_cptr;
+			using page_ctype_ref	= typename base::page_ctype_ref;
+
+			using text_type		= typename base::text_type;
+			using text_type_ptr	= typename base::text_type_ptr;
+			using text_type_ref	= typename base::text_type_ref;
+			using text_ctype_ptr	= typename base::text_ctype_ptr;
+			using text_ctype_cptr	= typename base::text_ctype_cptr;
+			using text_ctype_ref	= typename base::text_ctype_ref;
+
+		public:
+
+			nik_ce corpus_literal() { }
+
+			template<typename B, auto N, typename P, auto M, typename T, auto L>
+			nik_ce corpus_literal(const B (&b)[N], const P (&p)[M], const T (&t)[L]) : base{b, p, t} { }
+	};
+
 /***********************************************************************************************************************/
 
 // interface:
+
+	template
+	<
+		typename Body, typename Page, typename Text, typename SizeType,
+		SizeType BodySize, SizeType PageSize, SizeType TextSize
+	>
+	class corpus	: public protocorpus // array_size(Page) <= array_size(Text)
+			<
+				cctmp::array < Body , SizeType , BodySize >,
+				cctmp::array < Page , SizeType , PageSize >,
+				cctmp::array < Text , SizeType , TextSize >
+			>
+	{
+		public:
+
+			using base		= protocorpus
+						<
+							cctmp::array < Body , SizeType , BodySize >,
+							cctmp::array < Page , SizeType , PageSize >,
+							cctmp::array < Text , SizeType , TextSize >
+						>;
+
+			using type		= typename base::type;
+			using type_ptr		= typename base::type_ptr;
+			using type_cptr		= typename base::type_cptr;
+			using type_ref		= typename base::type_ref;
+
+			using ctype		= typename base::ctype;
+			using ctype_ptr		= typename base::ctype_ptr;
+			using ctype_cptr	= typename base::ctype_cptr;
+			using ctype_ref		= typename base::ctype_ref;
+
+			using size_type		= typename base::size_type;
+			using size_ctype	= typename base::size_ctype;
+
+			using page_type		= typename base::page_type;
+			using page_type_ptr	= typename base::page_type_ptr;
+			using page_type_ref	= typename base::page_type_ref;
+			using page_ctype_ptr	= typename base::page_ctype_ptr;
+			using page_ctype_cptr	= typename base::page_ctype_cptr;
+			using page_ctype_ref	= typename base::page_ctype_ref;
+
+			using text_type		= typename base::text_type;
+			using text_type_ptr	= typename base::text_type_ptr;
+			using text_type_ref	= typename base::text_type_ref;
+			using text_ctype_ptr	= typename base::text_ctype_ptr;
+			using text_ctype_cptr	= typename base::text_ctype_cptr;
+			using text_ctype_ref	= typename base::text_ctype_ref;
+
+		protected:
+
+			nik_ce void upsize(size_ctype n, size_ctype m = 1) { base::operator [] (n).upsize(m); }
+
+			nik_ce void push_finish(size_ctype n, size_ctype text_start)
+			{
+				size_ctype text_finish = base::text.size();
+
+				upsize(n);
+				base::clast(n).set_start  (text_start);
+				base::clast(n).set_finish (text_finish);
+
+			}
+
+			nik_ce void set_finish(size_ctype n, size_ctype m, size_ctype text_start)
+			{
+				size_ctype text_finish = base::text.size();
+
+				base::at(n, m).set_start  (text_start);
+				base::at(n, m).set_finish (text_finish);
+			}
+
+		public:
+
+			nik_ce corpus() { }
+
+			template<typename B, auto N, typename P, auto M, typename T, auto L>
+			nik_ce corpus(const B (&b)[N], const P (&p)[M], const T (&t)[L]) : base{b, p, t} { }
+
+			// mutable:
+
+				using base::begin;
+				using base::end;
+				using base::last;
+				using base::at;
+				using base::iter;
+
+				// page:
+
+				nik_ce page_type_ptr begin (size_ctype n) { return base::page.iter(base::start(n)); }
+				nik_ce page_type_ptr end   (size_ctype n) { return base::page.iter(base::finish(n)); }
+				nik_ce page_type_ptr last  (size_ctype n) { return end(n) - 1; }
+
+				nik_ce page_type_ptr iter (size_ctype n, size_ctype m) { return begin(n) + m; }
+				nik_ce page_type_ref at   (size_ctype n, size_ctype m) { return begin(n)[m]; }
+
+				// text:
+
+				nik_ce text_type_ptr begin(size_ctype n, size_ctype m)
+					{ return base::text.iter(base::start(n, m)); }
+
+				nik_ce text_type_ptr end(size_ctype n, size_ctype m)
+					{ return base::text.iter(base::finish(n, m)); }
+
+				nik_ce text_type_ptr last(size_ctype n, size_ctype m) { return end(n) - 1; }
+
+				nik_ce text_type_ptr iter(size_ctype n, size_ctype m, size_ctype l)
+					{ return begin(n, m) + l; }
+
+				nik_ce text_type_ref at(size_ctype n, size_ctype m, size_ctype l)
+					{ return begin(n, m)[l]; }
+
+				template<typename In, typename End>
+				nik_ce void push(size_ctype n, In in, End end)
+				{
+					size_ctype text_start = base::text.size();
+
+					base::text.push(in, end);
+					push_finish(n, text_start);
+				}
+
+				template<typename F, typename In, typename End>
+				nik_ce void pushmap(size_ctype n, F f, In in, End end)
+				{
+					size_ctype text_start = base::text.size();
+
+					base::text.pushmap(f, in, end);
+					push_finish(n, text_start);
+				}
+
+				template<typename F, typename In, typename End>
+				nik_ce void setmap(size_ctype n, size_ctype m, F f, In in, End end)
+				{
+					size_ctype text_start = base::text.size();
+
+					base::text.pushmap(f, in, end);
+					set_finish(n, m, text_start);
+				}
+	};
 
 /***********************************************************************************************************************/
 /***********************************************************************************************************************/
